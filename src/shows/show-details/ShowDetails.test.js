a9fdee35d59149a30736f0ef238d959ec9c3d18f
dzianisroi <dzianis_roi@epam.com>
2021-02-02 12:39:51 +0000

    Add test for ShowDetails.js

---
+++ src/shows/show-details/ShowDetails.test.js
@@ -0,0 +1,30 @@
+import renderer from "react-test-renderer";
+
+import { ShowDetails } from "./ShowDetails";
+
+import show from "../../tvmaze-sdk/shows/__mocks__/showDetails.json";
+
+import { REQUEST_STATUS_FULFILLED } from "../../ui-lib/request-status/RequestStatus";
+
+let tree;
+let props;
+
+beforeEach(() => {
+    tree = renderer
+        .create(<ShowDetails {...props} />)
+        .toJSON();
+});
+
+describe(`when request status is "${REQUEST_STATUS_FULFILLED}" and there is a show`, () => {
+    beforeAll(() => {
+        props = {
+            episodesListElement: <div />,
+            requestStatus: REQUEST_STATUS_FULFILLED,
+            show,
+        };
+    });
+
+    test("should match snapshot", () => {
+        expect(tree).toMatchSnapshot();
+    });
+});
